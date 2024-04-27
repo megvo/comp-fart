f5ee0c229d002d7f81750e8bb0a3c12d577ba06f
lamiahkhan017 <160285909+lamiahkhan017@users.noreply.github.com>
2024-04-27 15:48:00 -0400

    Update sl2.sv

--- catalog/sl2/sl2.sv
+++ catalog/sl2/sl2.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: Prof Rob Marano
+// Engineer: Lamiah Khan and Megan Vo
 // 
 //     Create Date: 2023-02-07
 //     Module Name: sl2
