f4f333f79bc63ee25cfca695bd39917c20f093fb
lamiahkhan017 <160285909+lamiahkhan017@users.noreply.github.com>
2024-04-27 15:58:34 -0400

    Update tb_mux2.sv

--- catalog/mux2/tb_mux2.sv
+++ catalog/mux2/tb_mux2.sv
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
-// ECE 251 Spring 2024
-// Engineer: Prof Rob Marano
+// ECE 251 Spring 2023
+// Engineer: Lamiah Khan and Megan Vo
 // 
-//     Create Date: 2023-02-07
+//     Create Date: 2024-27-04
 //     Module Name: tb_mux2
 //     Description: Test bench for 2 to 1 multiplexer
 //
@@ -17,10 +17,10 @@
 `include "mux2.sv"
 `include "../clock/clock.sv"
 
-module tb_mux2;
+module tb_mux4;
     parameter n = 32; // #bits for an operand
-    logic s;
-    logic [(n-1):0] d0, d1;
+    logic [1:0] s;
+    logic [(n-1):0] d0, d1, d2, d3;
     logic [(n-1):0] y;
     wire clk;
     logic enable;
@@ -30,22 +30,23 @@ module tb_mux2;
         $dumpfile("mux2.vcd");
         $dumpvars(0, uut0, uut1);
         // $monitor("s = %0b d0 = (0x%0h)(%0d) d1 = (0x%0h)(%0d) y = (0x%0h)(%0d)", s, d0, d0, d1, d1, y, y);
-        $monitor("time=%0t \t enable=%0b s=%0b y=%h d0=%h d1=%h",$realtime, enable, s, y, d0, d1);
+        $monitor("time=%0t \t enable=%h s=%h y=%h d0=%h d1=%h d2=%h d3=%h",$realtime, enable, s, y, d0, d1, d2, d3);
     end
 
     initial begin
-        d0 <= #n'h80000000;
-        d1 <= #n'h00000001;
+        d0 <= #n'h00000001;
+        d1 <= #n'h00000002;
+        d2 <= #n'h00000003;
+        d3 <= #n'h00000005;
         enable <= 0;
         #10 enable <= 1;
-        #10 s <= 1'b0;
-        #20 s <= 1'b1;
+        #10 s = 2'b11;
         #100 enable <= 0;
         $finish;
     end
 
-    mux2 uut0(
-        .S(s), .D0(d0), .D1(d1), .Y(y)
+    mux4 uut0(
+        .S(s), .D0(d0), .D1(d1), .D2(d2), .D3(d3), .Y(y)
     );
     clock uut1(
         .ENABLE(enable),
