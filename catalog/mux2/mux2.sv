029aaa505eef31d4931914715f14fecd1d4c901d
lamiahkhan017 <160285909+lamiahkhan017@users.noreply.github.com>
2024-04-27 15:57:45 -0400

    Update mux2.sv

--- catalog/mux2/mux2.sv
+++ catalog/mux2/mux2.sv
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
-// ECE 251 Spring 2024
-// Engineer: Prof Rob Marano
+// ECE 251 Spring 2023
+// Engineer: Lamiah Khan and Megan Vo
 // 
-//     Create Date: 2023-02-07
+//     Create Date: 2024-27-04
 //     Module Name: mux2
 //     Description: 2 to 1 multiplexer
 //
@@ -28,6 +28,28 @@ module mux2
     // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
     //
     assign Y = S ? D1 : D0;
+endmodule
+
+module mux4 
+    #(parameter n = 32)(
+    //
+    // ---------------- PORT DEFINITIONS ----------------
+    //
+    input  logic [(n-1):0] D0, D1, D2, D3,
+    input  logic [1:0] S,
+    output logic [(n-1):0] Y
+);
+    //
+    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
+    //
+    always @*
+        case(S)
+            2'b00: Y = D0;
+            2'b01: Y = D1;
+            2'b10: Y = D2;
+            2'b11: Y = D3;
+        endcase
+        
 endmodule
 
 `endif // MUX2
