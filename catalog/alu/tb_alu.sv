50e460e556091df000bec9ac9d39c503691723c7
megvo <112580071+megvo@users.noreply.github.com>
2024-04-27 16:07:50 -0400

    Update tb_alu.sv

--- catalog/alu/tb_alu.sv
+++ catalog/alu/tb_alu.sv
@@ -1,23 +1,74 @@
-//////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Megan Vo and Lamiah Khan
 // 
-//     Create Date: 2023-02-07
-//     Module Name: tb_alu
-//     Description: Test bench for simple behavorial ALU
-//
-// Revision: 1.0
-//
-//////////////////////////////////////////////////////////////////////////////////
-`ifndef TB_ALU
-`define TB_ALU
+//     Create Date: 2024-04-27
 
-`timescale 1ns/100ps
 `include "alu.sv"
+`timescale 1ns/100ps
 
-module tb_alu;
-    parameter n = 32;
+module alu_tb();
 
+  // Testbench Inputs
+  reg clk;
+  reg [(31):0] a, b;
+  reg [2:0] alucontrol;
+
+  // Testbench Outputs
+  wire [(31):0] result;
+  wire zero;
+
+  // Instantiate ALU module
+  alu dut (
+  .clk(clk),
+  .a(a),
+  .b(b),
+  .alucontrol(alucontrol),
+  .result(result),
+  .zero(zero)
+  );
+
+  // Testbench Procedure
+ initial begin
+  // Initialize inputs
+   clk = 0;
+   a = 32'h00000000;
+   b = 32'h00000000;
+   alucontrol = 3'b000; // Branch (SLT) operation
+ end
+
+ always begin
+  #5 clk = ~clk;
+ end
+
+ initial begin
+  // Set up input values for the branch instruction
+  #100;
+  a = 32'h0000000B;
+  b = 32'h0000000B;
+  alucontrol = 3'b111;
+  #10;
+  $display("Result = %h", result);
+  $display("zero = %h", zero);
+  //$dumpfile("alu.vcd");
+  //$dumpvars(result,zero,a,b,alucontrol,clk);
+  #10; 
+  a = 32'h0000000B;
+  b = 32'h0000000A;
+  alucontrol = 3'b111;
+  #10;
+  $display("Result = %h", result);
+  $display("zero = %h", zero);
+  #10;
+    a = 32'h0000000A;
+  b = 32'h0000000B;
+  alucontrol = 3'b110;
+  #10;
+  $display("Result = %h", result);
+  $display("zero = %h", zero);
+
+  #100 $finish;
+ end
+  // Modify the input values as necessary for different test cases
+ 
 endmodule
-`endif // TB_ALU
