f91e6f5a135e84bea093cb756ec4364fcc57ad04
megvo <112580071+megvo@users.noreply.github.com>
2024-04-28 14:13:18 -0400

    Update tb_aludec.sv 2

--- catalog/aludec/tb_aludec.sv
+++ catalog/aludec/tb_aludec.sv
@@ -8,21 +8,19 @@
 
 module aludec_tb;
 
-  // Testbench Inputs
+
   reg [5:0] funct;
   reg [1:0] aluop;
-
-  // Testbench Outputs
   wire [2:0] alucontrol;
 
-  // Instantiate aludec module
+
   aludec #(.n(32)) dut (
     .funct(funct),
     .aluop(aluop),
     .alucontrol(alucontrol)
   );
 
-  // Testbench Procedure
+ 
   initial begin
     // Initialize inputs
     funct = 6'b000000;
@@ -49,10 +47,8 @@ module aludec_tb;
     #5; // Wait for output stabilization
     $display("ALU Control: %b", alucontrol);
 
-    // ...
-
-    // End simulation
     #100 $finish;
   end
 
 endmodule
+
