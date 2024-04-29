113442a446c4ac5b5760c6c725e3a2e01bb77508
megvo <112580071+megvo@users.noreply.github.com>
2024-04-29 16:36:51 -0400

    Update adder.sv

--- catalog/adder/adder.sv
+++ catalog/adder/adder.sv
@@ -16,18 +16,44 @@
 
 `timescale 1ns/100ps
 
-module adder
-    #(parameter n = 32)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic [(n-1):0] A, B,
-    output logic [(n-1):0] Y
+module adder #(
+    parameter WIDTH = 32  // Parameter for the width of the adder
+)(
+    input [WIDTH-1:0] A,
+    input [WIDTH-1:0] B,
+    output [WIDTH-1:0] Y
 );
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    assign Y = A + B;
+
+
+    // LSB Adder
+    full_adder fa0 (
+        .A(A[0]),
+        .B(B[0]),
+        .Y(Y[0])
+    );
+
+
+    genvar i;
+    generate
+        for (i = 1; i < WIDTH; i = i + 1) begin : full_adder_loop
+            full_adder fa (
+                .A(A[i]),
+                .B(B[i]),
+                .Y(Y[i])
+            );
+        end
+    endgenerate
+
+
 endmodule
 
 `endif // ADDER
+
+// One-bit full adder logic
+module full_adder(
+    input A,
+    input B,
+    output Y
+);
+    assign Y = A ^ B;
+endmodule
