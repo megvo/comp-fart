edc0fa2d3ac4a0f60f3731659bc8633c23ae691c
megvo <112580071+megvo@users.noreply.github.com>
2024-05-04 00:33:17 -0400

    Update cpu.sv

--- catalog/cpu/cpu.sv
+++ catalog/cpu/cpu.sv
@@ -12,44 +12,50 @@
 //////////////////////////////////////////////////////////////////////////////////
 `ifndef CPU
 `define CPU
-
 `timescale 1ns/100ps
 
-`include "../controller/controller.sv"
 `include "../datapath/datapath.sv"
+`include "../controller/controller.sv"
 
-module cpu
-    #(parameter n = 32)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic           clk, reset,
-    output logic [(n-1):0] pc,
-    input  logic [(n-1):0] instr,
-    output logic           memwrite,
-    output logic [(n-1):0] aluout, writedata,
-    input  logic [(n-1):0] readdata
-);
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
+module cpu (
+    input logic clk, rst,
+    input logic [15:0] instruction, readData,
+    output logic memWrite,
+    output logic [15:0] aluResult, memWriteData, pc
+    );
+    logic zero, regWrite, pcSrc;
+    logic [1:0] regDst, memToReg, jump, aluSrc;
+    logic [2:0] aluCtrl;
 
-    // cpu internal components
-    logic       alusrc, regwrite, pcsrc, zero;
-    logic [1:0] memtoreg, regdst, jump;
-    logic [2:0] alucontrol;
-    
-    controller c(instr[(31):26], instr[5:0], zero,
-                    memtoreg, memwrite, pcsrc,
-                    alusrc, regdst, regwrite, jump,
-                    alucontrol);
-
-    datapath dp(clk, reset, memtoreg, pcsrc,
-                    alusrc, regdst, regwrite, jump,
-                    alucontrol,
-                    zero, pc, instr,
-                    aluout, writedata, readdata);
-
+    controller cf_controller(
+        instruction[15:12],
+        zero,
+        regWrite,
+        memWrite,
+        pcSrc,
+        regDst,
+        memToReg,
+        jump,
+        aluSrc,
+        aluCtrl
+        );
+    datapath cf_datapath(
+        clk,
+        rst,
+        regWrite,
+        pcSrc,
+        regDst,
+        memToReg,
+        jump,
+        aluSrc,
+        aluCtrl,
+        instruction,
+        readData,
+        aluResult,
+        memWriteData,
+        pc,
+        zero
+        );
 endmodule
 
 `endif // CPU
