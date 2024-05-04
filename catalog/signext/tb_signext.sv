00d4dfa78d41d2f4a910153d23f5989eacc05e28
megvo <112580071+megvo@users.noreply.github.com>
2024-05-04 00:46:54 -0400

    Update tb_signext.sv

--- catalog/signext/tb_signext.sv
+++ catalog/signext/tb_signext.sv
@@ -10,31 +10,63 @@
 // Revision: 1.0
 //
 //////////////////////////////////////////////////////////////////////////////////
-`ifndef TB_SIGNEXT
-`define TB_SIGNEXT
-
 `timescale 1ns/100ps
-`include "../signext/signext.sv"
 
-module tb_sl2;
-    parameter n = 32; // #bits for an operand
-    parameter i = n/2; // #bits for an immediate
-    logic [(i-1):0] a;
-    logic [(n-1):0] y;
+`include "./signext.sv"
 
-   initial begin
-        $dumpfile("signext.vcd");
-        $dumpvars(0, uut);
-        //$monitor("a = %b (0x%0h)(%0d) y = %b (0x%0h)(%0d) ", a, a, a, y, y, y);
-        $monitor("time=%0t \t a=%b y=%b",$realtime, a, y);
+module signext_tb();
+    logic [3:0] in;
+    logic [15:0] out;
+
+    reg clk, reset; //reset for initializing testvectors
+
+    logic [19:0] testvectors[0:1000];
+    integer vectornum, errors;
+    logic [15:0] expectedOut;
+
+
+    signext uut (
+             .in(in),
+             .out(out)
+    );
+
+    always begin
+        clk = 1;
+        #5;
+        clk = 0;
+        #5;
     end
 
     initial begin
-        a <= #i'h8000;
+        $dumpfile("tb_signext.vcd");
+        $dumpvars(0, uut);
+    
+        testvectors[0] = {4'b1010, 16'hfffa};
+        vectornum = 0;
+        errors = 0;
+        reset = 1; #27; reset = 0;
     end
 
-    signext uut(
-        .A(a), .Y(y)
-    );
+    always @(posedge clk) begin
+        #1; 
+        {in, expectedOut} = testvectors[vectornum];
+    end
+
+    always @(negedge clk) begin
+        #1;
+        if (~reset) begin
+            if (out !== expectedOut) begin
+                $display("Error:\tinputs: in = %h", in);
+                $display("\tout = %h, expectedOut = %h", out, expectedOut);
+                errors = errors + 1;
+            end
+            vectornum = vectornum + 1;
+            if (testvectors[vectornum] === 20'hx) begin
+                $display("%d tests completed with %d errors", vectornum, errors);
+                $finish;
+            end
+        end
+    end
+
+
 endmodule
-`endif // TB_SIGNEXT
