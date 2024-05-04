761239e1dca93b918a42b7770d70dd4b61a03734
megvo <112580071+megvo@users.noreply.github.com>
2024-05-04 00:39:37 -0400

    Update maindec.sv

--- catalog/maindec/maindec.sv
+++ catalog/maindec/maindec.sv
@@ -11,56 +11,58 @@
 
 `ifndef MAINDEC
 `define MAINDEC
-
 `timescale 1ns/100ps
+`include "../alu/alu.sv"
 
-module maindec
-    #(parameter n = 32)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic [5:0] op,
-    output logic [1:0] memtoreg, 
-    output logic       memwrite,
-    output logic       branch, alusrc,
-    output logic [1:0] regdst, 
-    output logic       regwrite,
-    output logic [1:0] jump,
-    output logic [1:0] aluop,
-    input  logic [5:0] funct
-);
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    logic [11:0] controls; // 9-bit control vector
+module maindec (
+    input logic [3:0] op,
+    output logic regWrite, branch, memWrite,
+    output logic [1:0] regDst, memToReg, jump, aluSrc,
+    output logic [2:0] aluCtrl
+    );
 
-    // controls has 9 logical signals
-    assign {regwrite, regdst, alusrc, branch, memwrite,
-            memtoreg, jump, aluop} = controls;
+      `define MAINDEC_CTRL_ADD    4'b0000
+      `define MAINDEC_CTRL_SUB    4'b0001
+      `define MAINDEC_CTRL_MOVE   4'b0010
+      `define MAINDEC_CTRL_JR     4'b0011
+      `define MAINDEC_CTRL_AND    4'b0100
+      `define MAINDEC_CTRL_OR     4'b0101
+      `define MAINDEC_CTRL_SLL    4'b0110
+      `define MAINDEC_CTRL_SRL    4'b0111
+      `define MAINDEC_CTRL_SLT    4'b1000
+      `define MAINDEC_CTRL_BEQ    4'b1001
+      `define MAINDEC_CTRL_ADDI   4'b1010
+      `define MAINDEC_CTRL_SUBI   4'b1011
+      `define MAINDEC_CTRL_LW     4'b1100
+      `define MAINDEC_CTRL_SW     4'b1101
+      `define MAINDEC_CTRL_J      4'b1110
+      `define MAINDEC_CTRL_JAL    4'b1111
 
-    always @* begin
+    logic [13:0] ctrl;
+    assign {regWrite, regDst, branch, memWrite, memToReg, jump, aluSrc, aluCtrl} = ctrl;
+
+    always @*
         case(op)
-            6'b000000: begin
-                if(funct == 6'b000111)
-                    controls <= 12'b000000001000;    // Jr
-                else
-                    controls <= 12'b101000000010;    // RTYPE
-            end
+            `MAINDEC_CTRL_ADD:  ctrl <= {11'b10100000000, `ALU_CTRL_ADD};
+            `MAINDEC_CTRL_SUB:  ctrl <= {11'b10100000000, `ALU_CTRL_SUB};
+            `MAINDEC_CTRL_MOVE: ctrl <= {11'b10100000000, `ALU_CTRL_ADD};
+            `MAINDEC_CTRL_JR:   ctrl <= {11'b00000001000, `ALU_CTRL_ADD};
+            `MAINDEC_CTRL_AND:  ctrl <= {11'b10100000000, `ALU_CTRL_AND};
+            `MAINDEC_CTRL_OR:   ctrl <= {11'b10100000000, `ALU_CTRL_OR};
+            `MAINDEC_CTRL_SLL:  ctrl <= {11'b10100000010, `ALU_CTRL_SLL};
+            `MAINDEC_CTRL_SRL:  ctrl <= {11'b10100000010, `ALU_CTRL_SRL};
+            `MAINDEC_CTRL_SLT:  ctrl <= {11'b10100000000, `ALU_CTRL_SLT};
 
-            6'b000001: controls <= 12'b100100010000; // LW
-            6'b000010: controls <= 12'b000101000000; // SW
-            6'b000011: controls <= 12'b100100000000; // ADDI
-            6'b000100: controls <= 12'b100100000000; // SUBI
-            6'b000101: controls <= 12'b000010000001; // BEQ
-            6'b001001: controls <= 12'b000010000011; // BNE
-            6'b000111: controls <= 12'b000000000100; // J
-            6'b001000: controls <= 12'b110000100100; // Jal
-            default:   controls <= 12'bxxxxxxxxxxxx; // illegal operation
+            `MAINDEC_CTRL_BEQ:  ctrl <= {11'b00010000000, `ALU_CTRL_SUB};
+            `MAINDEC_CTRL_ADDI: ctrl <= {11'b10000000001, `ALU_CTRL_ADD};
+            `MAINDEC_CTRL_SUBI: ctrl <= {11'b10000000001, `ALU_CTRL_SUB};
+            `MAINDEC_CTRL_LW:   ctrl <= {11'b10000010001, `ALU_CTRL_ADD};
+            `MAINDEC_CTRL_SW:   ctrl <= {11'b00001000001, `ALU_CTRL_ADD};
+
+            `MAINDEC_CTRL_J:    ctrl <= {11'b00000000100, `ALU_CTRL_ADD};
+            `MAINDEC_CTRL_JAL:  ctrl <= {11'b11000100100, `ALU_CTRL_ADD};
+            default: ctrl <= 14'bx; // illegal
         endcase
-    end
-
-
-
 endmodule
 
 `endif // MAINDEC
