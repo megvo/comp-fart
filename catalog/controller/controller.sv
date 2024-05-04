5609ef983f44bba83e6d2ecb169898906c061432
megvo <112580071+megvo@users.noreply.github.com>
2024-05-04 00:32:39 -0400

    Update controller.sv

--- catalog/controller/controller.sv
+++ catalog/controller/controller.sv
@@ -11,41 +11,32 @@
 //////////////////////////////////////////////////////////////////////////////////
 `ifndef CONTROLLER
 `define CONTROLLER
-
 `timescale 1ns/100ps
 
 `include "../maindec/maindec.sv"
-`include "../aludec/aludec.sv"
 
-module controller
-    #(parameter n = 32)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic [5:0] op, funct,
-    input  logic       zero,
-    output logic [1:0] memtoreg, 
-    output logic       memwrite,
-    output logic       pcsrc, alusrc,
-    output logic [1:0] regdst, 
-    output logic       regwrite,
-    output logic [1:0] jump,
-    output logic [2:0] alucontrol
-);
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    logic [1:0] aluop;
-    logic       branch;
-    
-    // CPU main decoder
-    maindec md(op, memtoreg, memwrite, branch, alusrc, regdst, regwrite, jump, aluop, funct);
-    // CPU's ALU decoder
-    aludec ad(funct, aluop, alucontrol);
+module controller (
+    input logic [3:0] op,
+    input logic zero,
+    output logic regWrite, memWrite, pcSrc,
+    output logic [1:0] regDst, memToReg, jump, aluSrc,
+    output logic [2:0] aluCtrl
+    );
 
-  assign pcsrc = branch & zero;
 
+    logic branch;
+    maindec mainDecoder(
+        op,
+        regWrite,
+        branch,
+        memWrite,
+        regDst,
+        memToReg,
+        jump,
+        aluSrc,
+        aluCtrl
+        );
+    assign pcSrc = branch & zero;
 endmodule
 
 `endif // CONTROLLER
-
