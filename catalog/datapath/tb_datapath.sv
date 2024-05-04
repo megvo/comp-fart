97ade7b98c3d2c9caba62fe0cdcc9830f81c3cac
megvo <112580071+megvo@users.noreply.github.com>
2024-05-04 00:34:11 -0400

    Update tb_datapath.sv

--- catalog/datapath/tb_datapath.sv
+++ catalog/datapath/tb_datapath.sv
@@ -10,98 +10,91 @@
 // Revision: 1.0
 //
 //////////////////////////////////////////////////////////////////////////////////
-`ifndef TB_DATAPATH
-`define TB_DATAPATH
-`include "../datapath/datapath.sv"
-`include "../clock/clock.sv"
-`include "../controller/controller.sv"
 `timescale 1ns/100ps
 
-module datapath_tb;
-    parameter n = 32;
-    reg clk, reset, alusrc, pcsrc, regwrite;
-    reg [1:0] memtoreg, regdst, jump;
-    reg [2:0] alucontrol;
-    reg [(n-1):0] instr, readdata;
+`include "./datapath.sv"
 
-  // Define output signals
-    wire [31:0] pc, aluout, writedata;
-    
+module tb_datapath();
+    logic rst, regWrite, pcSrc;
+    logic [1:0] regDst, memToReg, jump, aluSrc;
+    logic [2:0] aluCtrl;
+    logic [15:0] instruction, readData;
 
-    logic [5:0] op, funct;
+    logic[15:0] aluResult, memWriteData, pc;
     logic zero;
 
-    // Testbench Outputs
-    logic memwrite;
-    //logic alusrc;
-    //logic regwrite;
-   // logic jump;
-   //logic [2:0] alucontrol;
+    reg clk, reset; //reset for initializing testvectors
 
-assign op=instr[31:26];
-assign funct=instr[5:0];
+    logic [139:0] testvectors[0:1000];
+    integer vectornum, errors;
+  
 
-    datapath dut1 (
-    .clk(clk),
-    .reset(reset),
-    .memtoreg(memtoreg),
-    .pcsrc(pcsrc),
-    .alusrc(alusrc),
-    .regdst(regdst),
-    .regwrite(regwrite),
-    .jump(jump),
-    .alucontrol(alucontrol),
-    .zero(zero),
-    .pc(pc),
-    .instr(instr),
-    .aluout(aluout),
-    .writedata(writedata),
-    .readdata(readdata)
-  );
+    logic [15:0] expectedAluResult, expectedMemWriteData, expectedPc;
+    logic expectedZero;
 
-    controller dut2 (
-        .op(op),
-        .funct(funct),
-        .zero(zero1),
-        .memtoreg(memtoreg),
-        .memwrite(memwrite),
-        .pcsrc(pcsrc),
-        .alusrc(alusrc),
-        .regdst(regdst),
-        .regwrite(regwrite),
-        .jump(jump),
-        .alucontrol(alucontrol)
+
+    datapath uut (
+             .clk(clk),
+             .rst(rst),
+             .regWrite(regWrite),
+             .regDst(regDst),
+             .memToReg(memToReg),
+             .jump(jump),
+             .aluSrc(aluSrc),
+             .pcSrc(pcSrc),
+             .aluCtrl(aluCtrl),
+             .instruction(instruction),
+             .readData(readData),
+             .aluResult(aluResult),
+             .memWriteData(memWriteData),
+             .pc(pc),
+             .zero(zero)
     );
-//   initial begin 
-//     $display("instr =%h zero =%h pc =%h alucontrol =%h aluout =%h writedata=%h", instr, zero, pc, alucontrol, aluout, writedata);
-//   end
+
+    always begin
+        clk = 1;
+        #5;
+        clk = 0;
+        #5;
+    end
+
+    initial begin
+        $dumpfile("tb_datapath.vcd");
+        $dumpvars(0, uut);
+        // 0_1_00_00_00_01_0_010_a047_0000___0007_XXXX_0002_0
+        
+        testvectors[0] = {1'b0, 1'b0, 2'b00, 2'b00, 2'b00, 2'b01, 1'b0, 3'b000, 16'h0000, 16'h0000, 16'h0000, 16'h0000, 16'hXXXX, 1'b1};
+        vectornum = 0;
+        errors = 0;
+        reset = 1; #27; reset = 0;
+    end
+
+    always @(posedge clk) begin
+        {rst, regWrite, regDst, memToReg, jump, aluSrc, pcSrc, aluCtrl, instruction, readData, expectedAluResult, expectedMemWriteData, expectedPc, expectedZero} = testvectors[vectornum];
 
 
-always #5 clk = ~clk;
 
-initial begin 
-clk=0;
-reset=1;
-#10;
-reset=0;
-//zero=0;
-instr = 32'h0C0D000F;  //addi $v1, $zero, 15
-#10;
-$display("instr =%h zero =%h pc =%h alucontrol =%h aluout =%h writedata=%h", instr, zero, pc, alucontrol, aluout, writedata);
-instr=  32'h0C0D000A;  //addi $v0, $zero, 10
-#10;
-$display("instr =%h zero =%h pc =%h alucontrol =%h aluout =%h writedata=%h ", instr, zero, pc, alucontrol, aluout, writedata);
-instr = 32'b00001001100000100000000000000100;  //store word  
-#10
- $display("instr =%h zero =%h pc =%h alucontrol =%h aluout =%h writedata=%h memwrite=%h", instr, zero, pc, alucontrol, aluout, writedata, memwrite);
- instr= 32'b00000101100000110000000000000100;
- //instr= 32'b00001000010011000000000000000100;  //load word
- #10;
- $display("instr =%h zero =%h pc =%h alucontrol =%h aluout =%h writedata=%h memtoreg=%h", instr, zero, pc, alucontrol, aluout, writedata, memtoreg);
-$finish;
-end
+    end
 
+    always @(negedge clk) begin
+        if (~reset) begin
+            if ({aluResult, memWriteData, pc, zero} !== {expectedAluResult, expectedMemWriteData, expectedPc, expectedZero}) begin
+                $display("Error:\tinputs: {rst,regWrite,regDst,memToReg,jump,aluSrc,pcSrc,aluCtrl} = %b %b %b %b %b %b %b %b", rst, regWrite, regDst, memToReg, jump, aluSrc, pcSrc, aluCtrl);
+                $display("      \tinstruction = %h, readData = %h", instruction, readData);
+                $display("\taluResult = %h, expectedAluResult = %h", aluResult, expectedAluResult);
+                $display("\tmemWriteData = %h, expectedMemWriteData = %h", memWriteData, expectedMemWriteData);
+                $display("\tpc = %h, expectedPc = %h", pc, expectedPc);
+                $display("\tzero = %b, expectedZero = %h", zero, expectedZero);
 
+                errors = errors + 1;
+            end
+            vectornum = vectornum + 1;
+            if (testvectors[vectornum] === 140'hx) begin
+                $display("%d tests completed with %d errors", vectornum, errors);
+                $finish;
+            end
+        end
+    end
 
 
 endmodule
