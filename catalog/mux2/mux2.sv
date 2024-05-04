3fe5ebaf94a3b8b624af23cad3ee38797e10c36d
megvo <112580071+megvo@users.noreply.github.com>
2024-05-04 00:40:14 -0400

    Update mux2.sv

--- catalog/mux2/mux2.sv
+++ catalog/mux2/mux2.sv
@@ -12,44 +12,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 `ifndef MUX2
 `define MUX2
-
 `timescale 1ns/100ps
 
 module mux2
-    #(parameter n = 32)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic [(n-1):0] D0, D1,
-    input  logic S,
-    output logic [(n-1):0] Y
-);
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    assign Y = S ? D1 : D0;
-endmodule
+    #(parameter n = 16) (
+    input logic [(n-1):0] a, b,
+    input logic s,
+    output logic [(n-1):0] result
+    );
 
-module mux4 
-    #(parameter n = 32)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic [(n-1):0] D0, D1, D2, D3,
-    input  logic [1:0] S,
-    output logic [(n-1):0] Y
-);
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    always @*
-        case(S)
-            2'b00: Y = D0;
-            2'b01: Y = D1;
-            2'b10: Y = D2;
-            2'b11: Y = D3;
-        endcase
-        
+    assign result = s ? b : a;
+
 endmodule
 
 `endif // MUX2
